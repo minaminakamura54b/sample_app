e3bc344850066756f5b233a8804a73b4fd482cb1
minaminakamura54b <160803250+minaminakamura54b@users.noreply.github.com>
2025-07-15 04:23:46 +0000

    created aplication, home about help

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
+
+#get /static_pages/home(URL)=>static_pages#home(コントローラー名#アクション名)
+#get /about => static_pages#about この方が見やすくて良い
   get 'static_pages/home'
   get 'static_pages/help'
-  root "hello#index"
+  get "static_pages/about"
 end
