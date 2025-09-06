132afdde2f391d851f0e5974e4ae371f6dc8b4cb
minaminakamura54b <160803250+minaminakamura54b@users.noreply.github.com>
2025-09-06 22:42:48 +0000

    basement finish

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
 
 #get /static_pages/home(URL)=>static_pages#home(コントローラー名#アクション名)
 #get /about => static_pages#about この方が見やすくて良い
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get "static_pages/about"
+  # get 'static_pages/home'
+  # get 'static_pages/help'
+  # get "static_pages/about"
+  # get "static_pages/contact"
+
+  get  "/help",    to: "static_pages#help"
+  get  "/about",   to: "static_pages#about"
+  get  "/contact", to: "static_pages#contact" 
+  get  "/signup",  to: "users#new"
 end
