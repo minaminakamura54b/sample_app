bb8f2755ec08d19fe9ecd02cdf64f3ef99776f04
Minami Nakamura <nakamura0373@gmail.com>
2026-03-31 20:56:49 -0700

    add

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "noreply@sandbox...mailgun.org"
+  default from: "noreply@sandbox71042557be214572950aee1fb345be83.mailgun.org"
   layout "mailer"
 end
